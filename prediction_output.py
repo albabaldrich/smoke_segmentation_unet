ca4f533c7ee09ae578c252d7fbbf81b729cfc639
albabaldrich <134705815+albabaldrich@users.noreply.github.com>
2023-07-21 14:34:43 +0200

    Update prediction_output.py

--- prediction_output.py
+++ prediction_output.py
@@ -21,6 +21,14 @@ if __name__ == "__main__":
     Images = LoadForPrediction(cfg, path='/home/ALBA/unet/smoke_dataset/goproNoIrShabeni1', ext='*.JPG')
 
     save_path = "/home/ALBA/unet/predictions_ronan/"
-    SavePredictions_new(model, Images, cfg, save_path)
+
+    '''Save original image + predicted mask + overlayed'''
+    path_pred = os.path.join(save_path, "pred_comparision")
+    SavePredictions_new(model, Images, cfg, path_pred)
+
+    '''Save only the predicted mask'''
+    mask_path = os.path.join(save_path, "masks")
+    Save_only_predict(model, Images, cfg, mask_path)
+    
 
     
